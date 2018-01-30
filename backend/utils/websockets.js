f27d99f1babf7700152379fdc8c4605a6232aabb
Jamie Lynch <James.Lynch@sky.uk>
2018-01-30 21:10:25 +0000

    fix error with chrome dumping connection on refresh

--- backend/utils/websockets.js
+++ backend/utils/websockets.js
@@ -5,6 +5,12 @@ var wss
 exports.createWebSocketServer = function(server) {
   wss = new WebSocket.Server({ server })
 
+  wss.on('connection', ws => {
+    ws.on('error', () => {
+      console.error('ws error')
+    })
+  })
+
   wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
